705fe51d23de25545029fe0bf35eca825365c635
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2022-02-11 11:36:04 -0700

    Added password, and returned a proper HTTP Status

--- src/main/java/com/xyzcorp/LoginResource.java
+++ src/main/java/com/xyzcorp/LoginResource.java
@@ -5,6 +5,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/login")
 public class LoginResource {
@@ -12,7 +13,13 @@ public class LoginResource {
     @GET()
     @Path("/{username}/{password}")
     @Produces(MediaType.TEXT_PLAIN)
-    public String helloToUser(@PathParam("username") String username) {
-        return "Hello, username, password is verified";
+    public Response helloToUser(@PathParam("username") String username, @PathParam("password") String password) {
+        if (username.equals("admin") && password.equals("pilot")) {
+
+            //Negotiate with business service
+            //Then the business service would talk to the database through DAO
+            return Response.ok().build();
+        }
+        return Response.status(401).build();
     }
 }
