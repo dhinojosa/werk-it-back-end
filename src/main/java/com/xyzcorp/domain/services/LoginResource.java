43c2995e4a284b7d58411afa1d0509cebb3c74d7
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2022-02-14 04:49:51 -0700

    Updated LoginResource to use database

--- src/main/java/com/xyzcorp/domain/services/LoginResource.java
+++ src/main/java/com/xyzcorp/domain/services/LoginResource.java
@@ -1,13 +1,17 @@
 package com.xyzcorp.domain.services;
 
+import com.xyzcorp.domain.models.User;
+
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Path("/login")
 public class LoginResource {
@@ -17,13 +21,17 @@ public class LoginResource {
 
     @GET()
     @Path("/{username}/{password}")
-    @Produces(MediaType.TEXT_PLAIN)
-    public Response helloToUser(@PathParam("username") String username, @PathParam("password") String password) {
-        if (username.equals("admin") && password.equals("pilot")) {
-            //Negotiate with business service
-            //Then the business service would talk to the database through DAO
-            return Response.ok().build();
-        }
-        return Response.status(401).build();
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response login(@PathParam("username") String username,
+                          @PathParam("password") String password) {
+        TypedQuery<User> query = entityManager.createQuery("Select u from " +
+            "User as u " +
+            "where u.username = :username and u.password = :password",
+            User.class);
+        query.setParameter("username", username);
+        query.setParameter("password", password);
+        List<User> resultList = query.setMaxResults(1).getResultList();
+        if (resultList.isEmpty()) return Response.status(401).build();
+        return Response.ok(resultList.get(0)).build();
     }
 }
